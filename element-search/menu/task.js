0f0750f7f9a00a5f9ca1b6b42bc085e52c1ee50d
Maksim L <rezerv@net.com>
2023-03-22 22:45:05 +0400

    finish

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,22 +1,34 @@
 const menuLink = document.getElementsByClassName('menu__link');
 const menuItem = document.getElementsByClassName('menu__item');
+const menuMain = document.getElementsByClassName('menu_main');
 
 for (let i = 0; i < menuLink.length; i++) {
-
-	let pr = menuItem[i].querySelector('.menu_sub');
-
-	if (pr !== null) {
-
+	
+	let menuSub = menuItem[i].querySelector('.menu_sub');
+	
+	if (menuSub !== null) {
+		
 		menuLink[i].onclick = function () {
+			
+			if (menuSub.className == 'menu menu_sub') {
 
-			if (pr.className == 'menu menu_sub') {
-				pr.className = 'menu menu_sub menu_active';
+				for (let i = 0; i < menuMain.length; i++) {
+
+					let pr = menuMain[i].querySelector('.menu_active');
+
+					if (pr !== null) {
+						pr.className = 'menu menu_sub';
+					}
+
+				}
+
+				menuSub.className = 'menu menu_sub menu_active';
 				return false;
+
 			} else {
-				pr.className = 'menu menu_sub';
+				menuSub.className = 'menu menu_sub';
 				return false;
 			}
-
 		}
 	}
 }
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,23 +1,23 @@
 const modalWindowAfter = document.getElementById('modal_main');
 modalWindowAfter.className = 'modal modal_active';
 
-const btnClickClose = document.getElementsByClassName('modal__close');
+const modalClose = document.getElementsByClassName('modal__close');
 const modalWindowBefore = document.getElementById('modal_success');
 
-for (let i = 0; i < btnClickClose.length; i++) {
+for (let i = 0; i < modalClose.length; i++) {
 
-    btnClickClose[i].onclick = function () {
+	modalClose[i].onclick = function () {
 
-        modalWindowAfter.className = 'modal';
+		modalWindowAfter.className = 'modal';
 
-        if (btnClickClose[i].classList.contains('show-success') == true) {
-            modalWindowBefore.className = 'modal modal_active';
-        }
+		if (modalClose[i].classList.contains('show-success') == true) {
+			modalWindowBefore.className = 'modal modal_active';
+		}
 
-        if (btnClickClose[i].classList.contains('modal__close_times') == true) {
-            modalWindowBefore.className = 'modal';
-        }
-    }
+		if (modalClose[i].classList.contains('modal__close_times') == true) {
+			modalWindowBefore.className = 'modal';
+		}
+	}
 }
 
 
